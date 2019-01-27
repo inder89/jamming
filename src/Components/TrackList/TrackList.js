b2920a098c5ac808c399487be1ab857224c5d93a
Inderpal <inderpal.chauhan@gmail.com>
2019-01-27 22:15:09 +1100

    functions are not valid as a Child bug

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,21 +4,24 @@ import Track from '../Track/Track';
 
  class TrackList extends Component {
     render() {
-        return(
-            <div className="TrackList">
-                {console.log(this.props.tracks)}
-                    { 
-                       
+       
+        return this.props.tracks ? 
+            (
+                <div className="TrackList">
+                    {console.log(this.props.tracks)}
+                        { 
                             this.props.tracks.map((track) => { 
                                 return (<Track track={track} key={track.id} />)
                             })
+                        }
+                            
                         
-                    }
-                    
-                
-            </div>
-        );
+                </div>
+            )
+        :
+            null;
     }
 }
+
 export default TrackList;
 
