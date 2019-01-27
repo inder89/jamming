f67c717d6cee91a3eb29b939c73cbf387038646f
Inderpal <inderpal.chauhan@gmail.com>
2019-01-27 21:59:47 +1100

    rendering tracks

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -20,10 +20,10 @@ class Track extends Component{
             <div className="Track">
                 <div className="Track-information">
                     <h3>{this.props.track.name}</h3>
-                    <p>{this.props.track.artist} | {this.props.track.album} --></p>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 
-                <a className="Track-action">{this.renderAction}</a>
+                <button className="Track-action">{this.renderAction}</button>
             </div>
         );
     }
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import './TrackList.css';
 import Track from '../Track/Track';
 
-class TrackList extends Component {
+ class TrackList extends Component {
     render() {
         return(
             <div className="TrackList">
-
-                {    
-                    this.props.tracks.map(track => { 
-                        return <Track track={track} key={track.id} />;
-                    })
-                }
+                {console.log(this.props.tracks)}
+                    { 
+                       
+                            this.props.tracks.map((track) => { 
+                                return (<Track track={track} key={track.id} />)
+                            })
+                        
+                    }
+                    
+                
             </div>
         );
     }
 }
-
 export default TrackList;
+
